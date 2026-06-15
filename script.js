1a84a48719501b8637c92c964fd45741bb500fff
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-06-15 15:23:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,56 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+	if(video.paused){
+		video.play();
+	}else{
+		video.pause();
+	}
+}
+
+function updateButton(){
+  const icon = video.paused ? '►' : '❚ ❚';
+  toggle.innerHTML = icon
+}
+
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+	 video[this.name] = this.value;
+}
+
+function handleProgress(){
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+
+toggle.addEventListener('click',togglePlay);
+video.addEventListener('play',updateButton);
+video.addEventListener('pause',updateButton);
+video.addEventListener('click',togglePlay);
+
+video.addEventListener('timeupdate',handleProgress)
+
+skipButtons.forEach((button=>button.addEventListener('click',skip)))
+
+// volume and speed 
+ranges.forEach(range => range.addEventListener('change',handleRangeUpdate));
+range.forEach(range => range.addEventListener('mousemove',handleRangeUpdate));
+
+
+
+
+
+
+
+
+
+
+
+
+
